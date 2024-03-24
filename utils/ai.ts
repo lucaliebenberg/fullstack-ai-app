0f94ae66092d774b4794396a1047b6f2e05ad6f8
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-03-24 18:25:27 +0200

    updated ai API changes

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,4 +1,15 @@
 import { OpenAI } from "langchain/llms/openai"
+import { z } from "zod"
+import { StructuredOutputParser } from "langchain/output_parsers"
+
+const schema = StructuredOutputParser.fromZodSchema(
+    z.object({
+        mood: z.string().describe('the mood of the person who wrote the journal entry.'),
+        summary: z.string().describe('quick summary of the entire entry.'),
+        negative: z.boolean().describe('is the journal entry negative? (i.e does it contain negative emotions?).'),
+        color: z.string().describe('a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'),
+    })
+)
 
 export const analyze = async (prompt) => {
     const model = new OpenAI({temperature: 0, modelName: 'gpt-3.5-turbo'})
