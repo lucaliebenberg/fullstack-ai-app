69acb350c059fae8ef4030f0de169ef82635a4a9
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-03-26 19:32:01 +0200

    discard this

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,8 +1,29 @@
-const History = () => {
+import HistoryChart from '@/components/History'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getData = async () => {
+    const user = await getUserByClerkID()
+    const analyses = await prisma.entryAnalysis.findMany({
+      where: {
+        userId: user.id,
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+}
+
+const History = async () => {
+    const { avg, analyses } = await getData();
+
     return (
-    <>
-        <div>History</div>
-    </>
+        <div className="w-full h-full">
+            <div className="w-full h-full">{`Avg. Sentiment ${avg}`}</div>
+            <div className="w-full h-full">
+                <HistoryChart data={analyses} />
+            </div>
+        </div>
     )
 }
 
